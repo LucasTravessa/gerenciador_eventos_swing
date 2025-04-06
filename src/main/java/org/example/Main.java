69527b9b9cdb1b455c1bb57f258ff83874297720
LucasTravessa <lucas.cos.travessa@gmail.com>
2025-04-06 20:01:35 -0300

    feat main running instance

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.example.controller.MainController;
+import org.example.view.MainView;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Cria uma instância da interface gráfica principal (MainView) que contém as abas
+        MainView mainView = new MainView();
+
+        // Cria uma instância do controlador que conecta a interface com a lógica do programa
+        // Supondo que o EventoController agora irá gerenciar as várias views
+        @SuppressWarnings("unused")
+        MainController controller = new MainController(mainView);
     }
 }
